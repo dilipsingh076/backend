967034736b1277d965ab28044725bcf080a3749c
Dilip Singh <101004823+dilipsingh076@users.noreply.github.com>
2023-01-16 15:59:42 +0530

    Delete SocialMediaAppbackend directory

--- SocialMediaAppbackend/config/db.js
+++
@@ -1,7 +0,0 @@
-const { mongoose } = require("mongoose");
-const mangoose = require("mongoose");
-
-const MONGO_URL = `mongodb+srv://dilipsinghf:7665135229@cluster0.mwlyv7a.mongodb.net/SocialMediaApp?retryWrites=true&w=majority`;
-const connection = mongoose.connect(MONGO_URL);
-
-module.exports = { connection };
